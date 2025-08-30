fe534c48010b04f462704526570c126a433a1d41
Ruhan Khandakar <khandakarruhan@gmail.com>
2025-08-30 19:22:57 +0600

    feat: ui load

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from agent-chat-bot!")
+import streamlit as st
+from ui.streamlit_ui.loadui import LoadStreamlitUI
+
+def load_langgraph_agentic_ai_app():
+    ui = LoadStreamlitUI()
+    user_input = ui.load_streamlit_ui()
+
+    if not user_input:
+        st.error("Error: failed to load user input from the UI")
+        return
+    
+    user_message = st.chat_input("Enter your message")
 
 
 if __name__ == "__main__":
-    main()
+    load_langgraph_agentic_ai_app()
